34536e6f8a2792583787955d517e7f1790c77101
Anastasiia-Berehovykh <anastanber@gmail.com>
2023-03-05 11:32:31 +0200

    2 task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,18 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+
+const onPlay = function (data) {
+  // data is an object containing properties specific to that event
+  localStorage.setItem('videoplayer-current-time', data.seconds);
+};
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+
+const pausedTime = localStorage.getItem('videoplayer-current-time');
+
+player.setCurrentTime(pausedTime);
