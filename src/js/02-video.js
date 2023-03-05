6e3d5176d144d8013c829a2c8c1c9b8da9aabb27
Anastasiia-Berehovykh <anastanber@gmail.com>
2023-03-05 13:28:23 +0200

    3 task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -8,6 +8,8 @@ const player = new Player(iframe);
 const onPlay = function (data) {
   // data is an object containing properties specific to that event
   localStorage.setItem('videoplayer-current-time', data.seconds);
+  
+  
 };
 
 player.on('timeupdate', throttle(onPlay, 1000));
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,45 @@
+import throttle from 'lodash.throttle';
+
+const formRef = document.querySelector('.feedback-form');
+const inputEmailRef = document.querySelector('.feedback-form input');
+const inputMessageRef = document.querySelector('.feedback-form textarea');
+const STORAGE_KEY = 'feedback-form-state';
+const formData = {};
+let receivedData;
+
+restoreInputsData();
+
+formRef.addEventListener('input', throttle(saveInputsData, 500));
+formRef.addEventListener('submit', onFormSubmit);
+
+function saveInputsData() {
+
+  formData.email = inputEmailRef.value;
+  formData.message = inputMessageRef.value;
+  
+  receivedData = formData;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+  
+}
+
+function restoreInputsData() {
+  const restoredFormData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+  if (restoredFormData) {
+    inputEmailRef.value = restoredFormData.email;
+    inputMessageRef.value = restoredFormData.message;
+
+  }
+  receivedData = restoredFormData;
+ 
+  
+}
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  console.log(receivedData);
+
+  event.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
+}
+
 
